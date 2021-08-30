717a4836725fea752b6bdaed5c3b4118d1a22b6d
Garvitraj Pandey <pandeygarvitraj@outlook.com>
2021-08-30 17:26:25 +0530

    Add files via upload

---
+++ Day 02/src/main/java/com/example/application/Application.java
@@ -0,0 +1,32 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.vaadin.artur.helpers.LaunchUtil;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/rubik", version = "4.5.0")
+@Theme(value = "formlayout", variant = Lumo.DARK)
+@PWA(name = "Form Layout", shortName = "Form Layout", offlineResources = {"images/logo.png"})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
